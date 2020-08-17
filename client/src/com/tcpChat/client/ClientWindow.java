0c1ac87cdedf8e67b4f7ac58bb9ab22c08017952
stroitel201 <61136643+stroitel201@users.noreply.github.com>
2020-08-18 02:54:31 +0300

    Update ClientWindow.java

--- client/src/com/tcpChat/client/ClientWindow.java
+++ client/src/com/tcpChat/client/ClientWindow.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 
 public class ClientWindow extends JFrame implements ActionListener, TCPConnectionListner {
 
-    private static final String IP_ADDR ="192.168.56.1";
+    private static final String IP_ADDR ="U wanna some ip?";
     private static final int PORT = 8189;
     private static final int WIDTH = 600;
     private static final int HEIGHT = 400;
